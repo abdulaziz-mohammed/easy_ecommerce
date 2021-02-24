a33a803a537aeabd52c630d6aa00e96face78da0
az-9 <az-9@live.com>
2021-02-24 15:30:55 +0300

    changes

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,35 +1,19 @@
 import 'package:meta/meta.dart';
+import 'package:rxdart/rxdart.dart';
 import '../models/models.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'service_base.dart';
 
 class UserService extends ServcieBase {
-  String _token;
-
   static final UserService instance = new UserService();
 
-  _loadToken() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    _token = prefs.getString('token');
-  }
-
-  _saveToken() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setString('token', _token);
-  }
-
-  _setToken(String token) async {
-    this._token = token;
-    _saveToken();
-  }
-
   Future<bool> isAuthinticated() async {
-    await _loadToken();
-    if (_token != null && _token.length > 0) return true;
+    return await client.getToken(username, password);
+
+    var token = await getToken();
+    if (token != null && token.length > 0) return true;
     return false;
   }
-
-  String getToken() => _token;
 
   Future<Customer> authenticate({
     @required String username,
@@ -40,11 +24,13 @@ class UserService extends ServcieBase {
 
     var customer = await client.getCustomer(token);
 
+    // onAuthnticated.add(true);
+
     return customer;
   }
 
-  deleteToken() async {
-    this._token = null;
-    await _saveToken();
+  logout() async {
+    await _setToken(null);
+    // onAuthnticated.add(false);
   }
 }
